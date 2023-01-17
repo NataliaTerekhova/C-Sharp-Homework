c2b2af508042a07fa7b29d9c62297483227ad823
Natalia <natalia_terekhova@yahoo.com>
2023-01-17 14:40:54 +0300

    Added Task 4

---
+++ Homework1Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+System.Console.Write("Введите число: ");
+int N=Convert.ToInt32(Console.ReadLine());
+int currentN=2;
+while (currentN<=N)
+{
+    System.Console.Write(currentN + " ");
+    currentN=currentN+2;
+}
